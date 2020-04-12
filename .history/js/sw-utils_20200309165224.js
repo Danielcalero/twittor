4e7f9d4e60137f85aa0f040834b16442cce54cd6
Daniel Calero <danielcalerocam@gmail.com>
2020-04-12 13:10:02 -0500

    first commit

---
+++ .history/js/sw-utils_20200309165224.js
@@ -0,0 +1,8 @@
+function actualizaCacheDinamico(dynamicCache,req,res){
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache =>{
+            cache.util(req,res.clone());
+            return res.clone();
+        })    
+    }
+}
---
+++ .history/sw_20200309155755.js
@@ -0,0 +1,3 @@
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
