83a44faa746be054210f979854057bd055d59e7f
Daniel Calero <danielcalerocam@gmail.com>
2020-04-12 14:08:36 -0500

    cambio ruta sw3

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var swLocation = '/twittor/sw.js'
 
 if(navigator.serviceWorker){
 
-    if(url.includes('localhost')){
+    if(location.includes('localhost')){
         swLocation='/sw.js'
     }
     navigator.serviceWorker.register(swLocation)
